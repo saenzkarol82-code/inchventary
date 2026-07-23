12de25f81ba9aad32644eb9aebc553592583243f
Karol Saenz Parra <karol.saenz@inchcape.com>
2026-07-23 09:35:55 -0500

    primer commit

---
+++ src/test/java/com/empresa/inventario_equipos/InventarioEquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.inventario_equipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioEquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
